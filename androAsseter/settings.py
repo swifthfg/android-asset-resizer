626afa235f7bceaec4dbbd90f357b9a3597717f9
faruk <gehafar@gmail.com>
2018-03-04 19:25:38 +0300

    Remove unnecessay lines

--- androAsseter/settings.py
+++ androAsseter/settings.py
@@ -76,13 +76,6 @@ WSGI_APPLICATION = 'androAsseter.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
-
-    """
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    """
 }
 
 
