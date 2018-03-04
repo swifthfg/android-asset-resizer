3e77cf2e4018cfdcdb98ee44493650cd555243e6
faruk <gehafar@gmail.com>
2018-03-04 19:23:11 +0300

    Change db sqlite to postgre

--- androAsseter/settings.py
+++ androAsseter/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'androAsseter.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
+
+    """
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    """
 }
 
 
